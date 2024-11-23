012c05625eeed134218df4819b532e604a52ff10
Jose Luis <wildchamo@gmail.com>
2024-11-23 14:54:20 -0500

    show todos on home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
-
+  todos = signal([
+    { name: 'Task 1', completed: false },
+    { name: 'Task 2', completed: true },
+    { name: 'Task 3', completed: false },
+  ]);
 }
