745a24e7aadfa419cab6a1a5c716372f9119914f
Jose Luis <wildchamo@gmail.com>
2024-11-27 12:22:08 -0500

    new code format

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,13 +6,13 @@ import {
   inject,
   Injector,
 } from '@angular/core';
-import { CommonModule } from '@angular/common';
+
 import { Todo } from '../../models/todos.model';
 import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [ReactiveFormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
